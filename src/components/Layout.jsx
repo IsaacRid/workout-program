3bf166b4f57e1c4cff7c8f5ddf540aeb1c91c26c
Isaac Rideal <isaac.rideal@gmail.com>
2025-09-12 18:16:31 +0100

    Added header

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,9 +2,16 @@ export default function Layout(props) {
 
     const { children } = props
 
+    const header = (
+        <header>
+            <h1 className="text-gradient">Workout Program</h1>
+            <p><strong>The 30 simple workouts program</strong></p>
+        </header>
+    )
 
     return (
         <>
+            {header}
             {children}
         </>
     )

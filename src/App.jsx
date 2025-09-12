422b6f71733ccf403ee66dc5fe634d1c991a2f60
Isaac Rideal <isaac.rideal@gmail.com>
2025-09-12 17:44:09 +0100

    Formatted app.jsx layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Layout from './components/Layout'
+import Grid from './components/Grid'
 
 function App() {
 
   return (
-    <>
-    </>
+    <Layout>
+      <main>
+        <Grid />
+
+      </main>
+    </Layout>
   )
 }
 
